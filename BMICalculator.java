6eaa6f08424193b9bb28554407e933fd64195fc9
linaboshra <60351601+linaboshra@users.noreply.github.com>
2020-02-13 13:36:53 -0500

    Update BMICalculator.java

--- BMICalculator.java
+++ BMICalculator.java
@@ -9,21 +9,21 @@ public class BMICalculator {
 
     public static void main(String[] args) {
 		
-		Scanner myObj = new Scanner(System.in);
+	Scanner myObj = new Scanner(System.in);
 	
-		System.out.println("Enter your weight in pounds");
+	System.out.println("Enter your weight in pounds");
 	
-		double weightKg = myObj.nextDouble() * 0.453592; //1 pound = 0.453592 kilograms
+	double weightKg = myObj.nextDouble() * 0.453592; //1 pound = 0.453592 kilograms
 		
-		System.out.println("Enter your height in feet");
+	System.out.println("Enter your height in feet");
 		
         double heightMeters = myObj.nextDouble() * 0.3048; //1 foot = 0.3048 meters
 		
-		double bodyMassIndex = weightKg / Math.pow(heightMeters, 2);
+	double bodyMassIndex = weightKg / Math.pow(heightMeters, 2);
 
         System.out.println("Your weight in kilograms is " + weightKg);
         System.out.println("Your height in meters is " + heightMeters);
-		System.out.println("Your BMI is " + bodyMassIndex);
+	System.out.println("Your BMI is " + bodyMassIndex);
     }
 
 }
