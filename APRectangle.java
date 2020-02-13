d9efe21ec2b1bee17edc2a3cb36e0fdfc62d0e86
linaboshra <60351601+linaboshra@users.noreply.github.com>
2020-02-13 13:13:08 -0500

    APRectangle.java

---
+++ APRectangle.java
@@ -0,0 +1,31 @@
+/*
+ * This program prompts the user to enter the dimensions of a rectangle and
+ * calculates its perimeter and area.
+ */
+
+import java.util.*;
+
+public class APRectangle {
+
+    public static void main(String[] args) {
+		
+		Scanner myObj = new Scanner(System.in);
+	
+		System.out.println("Enter the length of the rectangle");
+
+        double length = myObj.nextDouble();
+		
+		System.out.println("Enter the width of the rectangle");
+		
+        double width = myObj.nextDouble();
+
+        double area, perimeter;
+
+        area = length * width;
+        perimeter = (2*length) + (2*width);
+
+        System.out.println("The area of this rectangle is " + area);
+        System.out.println("The perimeter of this rectangle is " + perimeter);
+    }
+
+}
