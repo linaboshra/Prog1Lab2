2f8574928a0752cb0b9b3202567621fd8da76ae2
linaboshra <60351601+linaboshra@users.noreply.github.com>
2020-02-13 18:17:42 -0500

    Update APRectangle.java

--- APRectangle.java
+++ APRectangle.java
@@ -9,23 +9,23 @@ public class APRectangle {
 
     public static void main(String[] args) {
 		
-		Scanner myObj = new Scanner(System.in);
+	Scanner myObj = new Scanner(System.in);
 	
-		System.out.println("Enter the length of the rectangle");
+	System.out.println("Enter the length of the rectangle");
 
-        	double length = myObj.nextDouble();
+        double length = myObj.nextDouble();
 		
-		System.out.println("Enter the width of the rectangle");
+	System.out.println("Enter the width of the rectangle");
 		
-        	double width = myObj.nextDouble();
+        double width = myObj.nextDouble();
 
-        	double area, perimeter;
+        double area, perimeter;
 
-        	area = length * width;
-        	perimeter = (2*length) + (2*width);
+        area = length * width;
+        perimeter = (2*length) + (2*width);
 
-        	System.out.println("The area of this rectangle is " + area);
-        	System.out.println("The perimeter of this rectangle is " + perimeter);
+        System.out.println("The area of this rectangle is " + area);
+        System.out.println("The perimeter of this rectangle is " + perimeter);
     }
 
 }
