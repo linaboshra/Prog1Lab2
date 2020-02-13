9cbedff510e11b6d90028e89323fe59bd09571eb
linaboshra <60351601+linaboshra@users.noreply.github.com>
2020-02-13 18:16:21 -0500

    ConvertDistance.java

---
+++ ConvertDistance.java
@@ -0,0 +1,27 @@
+/*
+ * This program prompts the user to enter a value of distance in meters and
+ * converts it into feet.
+ */
+
+import java.util.*;
+
+public class ConvertDistance {
+
+    public static void main(String[] args) {
+		
+		Scanner myObj = new Scanner(System.in);
+	
+		System.out.println("Enter the value of distance in meters you want to convert");
+
+        double distanceMeters = myObj.nextDouble();
+
+        double distanceFeet;
+         
+        final double metersToFeet = 0.3048;
+
+        distanceFeet = distanceMeters * metersToFeet;
+
+        System.out.println( distanceMeters + " meters equals " + distanceFeet + " feet");
+    }
+
+}
